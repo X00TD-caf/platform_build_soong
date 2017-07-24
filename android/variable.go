0189bfef5a336dffbd59483c29c0dd3c5ef26aae
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:32:38 +0200

    soong: Add has_legacy_camera_hal1 product variable
    Change-Id: I9c430bf5375b7adf2bbf273b76809c796cd6ac7d
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -125,6 +125,10 @@ type variableProperties struct {
 			Cflags []string
                 }
 
+		Has_legacy_camera_hal1 struct {
+			Cflags []string
+		}
+
 		Needs_text_relocations struct {
 			Cppflags []string
 		}
@@ -255,6 +259,7 @@ type productVariables struct {
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
+	Has_legacy_camera_hal1 *bool `json:",omitempty"`
 	Needs_text_relocations *bool `json:",omitempty"`
 	Target_shim_libs *string `json:",omitempty"`
 	Uses_qcom_bsp_legacy *bool `json:",omitempty"`
